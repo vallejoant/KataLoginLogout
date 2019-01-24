12534de69b1d671f16a3ebe63cf1505432897128
David Vallejo <dvallejo@MacBoo-Pro-OnTruck-David.local>
2019-01-24 15:52:15 +0100

    Project creation

---
+++ KataLoginLogout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KataLoginLogout
+//
+//  Created by David Vallejo on 24/01/2019.
+//  Copyright © 2019 David Vallejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
